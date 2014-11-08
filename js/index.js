f4edd26667d9b3ce0d4d4eaca188e11135b40717
Jaesung Ryu <subsyoo@gmail.com>
2014-11-08 19:33:59 +0900

    search box 생성 및 기능 추가

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ var init = function(){
 	$("#loginBox").fadeIn(1200); // login box fade in
 	
 	initSignupFunctions();
+	initContentsFunctions();
 	
 	$("#headerLogOut").click(function(){
 		/**
@@ -42,6 +43,10 @@ var init = function(){
 			return;
 		}
 		
+		/**
+		 * TODO
+		 * 서버에서 login validation 체크 필요 
+		 */
 		$("#headerSignUp").hide();
 		$("#headerLogOut").show();
 		$("#backgroundBlur").fadeOut(700, function() {});
@@ -67,95 +72,6 @@ var init = function(){
 		console.log("click privacy");
 	});
 	
-	/**
-	 * 상단 탭 버튼 누를 시 div 교체 
-	 */
-	$(".mainPageHeaders").click(function(){
-		switch($(this).attr("id")){
-		case "mainPageHeaderMyLibrary":
-			$("#mainPageContentsMyLibrary").show();
-			$("#mainPageContentsStore").hide();
-			$("#mainPageContentsHWPurchase").hide();
-			
-			$("#paymentBox").hide();
-			$("#descriptionBox").hide();
-			$("#validPwBox").hide();
-			break;
-		case "mainPageHeaderStore":
-			$("#mainPageContentsMyLibrary").hide();
-			$("#mainPageContentsStore").show();
-			$("#mainPageContentsHWPurchase").hide();
-			
-			$("#paymentBox").hide();
-			$("#descriptionBox").hide();
-			$("#validPwBox").hide();
-			break;
-		case "mainPageHeaderHWPurchase":
-			$("#mainPageContentsMyLibrary").hide();
-			$("#mainPageContentsStore").hide();
-			$("#mainPageContentsHWPurchase").show();
-			
-			$("#backgroundBlur").fadeIn(1000,function(){});
-			$("#descriptionBox").fadeIn(1000,function(){});
-			$("#paymentBox").hide();
-			$("#validPwBox").hide();
-			break;
-		}
-	});
-	
-	/**
-	 * 콘텐츠 클릭 시 이벤트 
-	 */
-	$(".mainPageContentsItems").click(function(){
-		var item = $(this);
-		console.log(item.children()[1].textContent + " clicked.");
-	});
-	
-    // valid the password
-	$("#mainPageHeaderSettings").click(function(){
-		$("#paymentBox").hide();
-		$("#descriptionBox").hide();
-		$("#backgroundBlur").show();
-		$("#validPassword").val("");
-		$("#validPwBox").fadeIn(1200, function() {});
-	});
-	
-	/*
-	 * 경필
-	 */
-    $("#validBoxEnter").click(function(){
-        var validPw = $("#validPassword").val();
-        if(pw == validPw)
-        {
-            $("#memberIdBox").val(id);
-            $("#validPwBox").hide();
-            $("#infoSettingBox").show();
-        }else {
-            alert("You input the wrong password!");
-            $("#validPwBox").hide();
-            $("#backgroundBlur").hide();
-        }
-    });
-    /*
-     * 경필
-     */
-    
-    /*
-     * 경미 
-     */
-	$("#hwPurchaseButton").click(function(){
-		$("#descriptionBox").fadeOut(500,function(){
-			$("#paymentBox").fadeIn(1000,function(){});
-		});
-	});
-
-	$("#payCheckOut").click(function(){
-		$("#paymentBox").fadeOut(1000,function(){});
-		$("#backgroundBlur").fadeOut(1000, function() {});
-	});
-	/*
-	 * 경미
-	 */
 };
 
 var initSignupFunctions = function(){
@@ -219,4 +135,150 @@ var initSignupFunctions = function(){
 		$("#signupBox").hide();
 		$("#loginBox").fadeIn(500);
 	});
+};
+
+var currentTabPage = "mainPageContentsMyLibrary"; // 처음 로그인 시 My Library 탭에 포커싱 되므로 My Library 로 초기화 
+
+var initContentsFunctions = function(){
+	/**
+	 * 상단 탭 버튼 누를 시 div 교체 
+	 */
+	$(".mainPageHeaders").click(function(){
+		
+		switch($(this).attr("id")){
+		case "mainPageHeaderMyLibrary":
+			currentTabPage = "mainPageContentsMyLibrary";
+			$("#mainPageSearchContainer").show();
+			
+			$("#mainPageContentsMyLibrary").show();
+			$("#mainPageContentsStore").hide();
+			$("#mainPageContentsHWPurchase").hide();
+			
+			$("#paymentBox").hide();
+			$("#descriptionBox").hide();
+			$("#validPwBox").hide();
+			break;
+		case "mainPageHeaderStore":
+			currentTabPage = "mainPageContentsStore";
+			$("#mainPageSearchContainer").show();
+			
+			$("#mainPageContentsMyLibrary").hide();
+			$("#mainPageContentsStore").show();
+			$("#mainPageContentsHWPurchase").hide();
+			
+			$("#paymentBox").hide();
+			$("#descriptionBox").hide();
+			$("#validPwBox").hide();
+			break;
+		case "mainPageHeaderHWPurchase":
+			currentTabPage = "mainPageContentsHWPurchase";
+			$("#mainPageSearchContainer").hide();
+			
+			$("#mainPageContentsMyLibrary").hide();
+			$("#mainPageContentsStore").hide();
+			$("#mainPageContentsHWPurchase").show();
+			
+			$("#backgroundBlur").fadeIn(1000,function(){});
+			$("#descriptionBox").fadeIn(1000,function(){});
+			$("#paymentBox").hide();
+			$("#validPwBox").hide();
+			break;
+		}
+		
+		$("#mainPageContentsSearch").val("");
+		$(".mainPageContentsItems").show();
+		
+	});
+	
+	/**
+	 * 콘텐츠 클릭 시 이벤트 
+	 */
+	$(".mainPageContentsItems").click(function(){
+		var item = $(this);
+		console.log(item.children()[1].textContent + " clicked.");
+	});
+	
+    // valid the password
+	$("#mainPageHeaderSettings").click(function(){
+		$("#paymentBox").hide();
+		$("#descriptionBox").hide();
+		$("#backgroundBlur").show();
+		$("#validPassword").val("");
+		$("#validPwBox").fadeIn(1200, function() {});
+	});
+	
+	/*
+	 * 경필
+	 */
+    $("#validBoxEnter").click(function(){
+        var validPw = $("#validPassword").val();
+        if(pw == validPw)
+        {
+            $("#memberIdBox").val(id);
+            $("#validPwBox").hide();
+            $("#infoSettingBox").show();
+        }else {
+            alert("You input the wrong password!");
+            $("#validPwBox").hide();
+            $("#backgroundBlur").hide();
+        }
+    });
+    /*
+     * 경필
+     */
+    
+    /*
+     * 경미 
+     */
+	$("#hwPurchaseButton").click(function(){
+		$("#descriptionBox").fadeOut(500,function(){
+			$("#paymentBox").fadeIn(1000,function(){});
+		});
+	});
+
+	$("#payCheckOut").click(function(){
+		$("#paymentBox").fadeOut(1000,function(){});
+		$("#backgroundBlur").fadeOut(1000, function() {});
+	});
+	/*
+	 * 경미
+	 */
+	
+	contentsSearchInit();
+};
+
+/**
+ * search box 에 대한 이벤트 핸들러들 모음 
+ */
+var contentsSearchInit = function(){
+	
+	$("#mainPageContentsSearch").val("");
+	$("#mainPageContentsSearch").unbind("keyup");
+	$("#mainPageContentsSearch").keyup(function(e) {
+		
+		var searchValue = this.value.toLowerCase();
+		var itemList = $("#"+currentTabPage).children();
+		
+		$.each(itemList, function(i, v) {
+			var curItemTitle = v.children[1].textContent;
+			if(curItemTitle.toLowerCase().indexOf(searchValue) != -1){
+				$(this).show();
+			} else {
+				$(this).hide();
+			}
+		});
+		
+	});
+	
+	/**
+	 * search box 의 x 버튼을 눌렀을 때 이벤트 
+	 */
+	var onSearchHandler = function(){
+		$(".mainPageContentsItems").show();
+	};
+	
+	var searchBox = document.getElementById("mainPageContentsSearch");
+	searchBox.removeEventListener("search", onSearchHandler, false);
+	searchBox.addEventListener("search", onSearchHandler, false);
+	
 };
