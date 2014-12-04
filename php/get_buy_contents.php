058e40f2232e502b976cd955c54e5f2f6f8985d6
lirasie <kimkm1015@gmail.com>
2014-12-04 14:32:22 +0900

    구매한 contents 정보 반환

---
+++ php/get_buy_contents.php
@@ -0,0 +1,24 @@
+<?php
+
+	include "dbconn.php";
+
+	$uid=$_SESSION['uid'];
+	$query = "SELECT cid FROM user_contents_info WHERE uid='$uid'";
+	$result=$mysqli->query($query);
+
+	while($row=$result->fetch_array(MYSQLI_ASSOC)){
+		$cid=$row["cid"];
+		$q = "SELECT * FROM contents WHERE cid='$cid'";
+		$res = $mysqli->query($q);
+		$contents_row=$res->fetch_array(MYSQLI_ASSOC);
+
+		$rows[]=$contents_row;
+
+		$res->free();
+	}
+
+	echo json_encode($rows);
+
+	$result->free();
+	$mysqli->close();
+?>
